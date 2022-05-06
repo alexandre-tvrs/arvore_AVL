2a03a9d25d0a37a7175ee9f33be30b370ef709fb
Alexandre Tavares <71346377+alexandre-tvrs@users.noreply.github.com>
2022-05-06 19:24:12 -0300

    First Commit

---
+++ arvoreBinaria.h
@@ -0,0 +1,45 @@
+typedef struct NO *arvAVL;
+
+arvAVL *cria_arvAVL();
+
+void liberar_arvAVL(arvAVL *raiz);
+
+void libera_NO(struct NO *no);
+
+int vazia_arvAVL(arvAVL *raiz);
+
+int alt_no(struct NO *no);
+
+int fatorBalanceamento_NO(struct NO *no);
+
+int maior(int x, int y);
+
+void rotacaoLL(arvAVL *raiz);
+
+void rotacaoRR(arvAVL *raiz);
+
+void rotacaoLR(arvAVL *raiz);
+
+void rotacaoRL(arvAVL *raiz);
+
+int altura_arvAVL(arvAVL *raiz);
+
+int totalNO_arvAVL(arvAVL *raiz);
+
+void preOrdem_arvAVL(arvAVL *raiz);
+
+void emOrdem_arvAVL(arvAVL *raiz);
+
+void posOrdem_arvAVL(arvAVL *raiz);
+
+/*
+
+int insere_arvAVL(arvAVL *raiz, int valor);
+
+int remove_arvAVL(arvAVL *raiz, int valor);
+
+struct NO *remove_atual(struct NO *atual);
+
+*/
+
+int consulta_arvAVL(arvAVL *raiz, int valor);
---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arvoreBinaria.h"
+
+int main()
+{
+    int x;
+    arvAVL *raiz;
+
+    raiz = cria_arvAVL();
+
+    /*
+
+    x = insere_arvAVL(raiz, 150);
+    x = insere_arvAVL(raiz, 110);
+    x = insere_arvAVL(raiz, 100);
+    x = insere_arvAVL(raiz, 130);
+    x = insere_arvAVL(raiz, 120);
+    x = insere_arvAVL(raiz, 140);
+    x = insere_arvAVL(raiz, 160);
+
+    */
+
+    if(vazia_arvAVL(raiz)){
+        printf("A arvore esta vazia.");
+    }else{
+        printf("A arvore possui elementos.");
+    }
+    printf("\n");
+
+    x = altura_arvAVL(raiz);
+    printf("Altura da arvore : %d\n", x);
+
+    x = totalNO_arvAVL(raiz);
+    printf("Total de nos na arvore : %d\n", x);
+
+    printf("Consulta em pre Ordem : \n");
+    preOrdem_arvAVL(raiz);
+    printf("Consulta em Ordem : \n");
+    emOrdem_arvAVL(raiz);
+    printf("Consulta em pos Ordem : \n");
+    posOrdem_arvAVL(raiz);
+
+    printf("\nBusca na Arvore Binaria: \n");
+    if(consulta_arvAVL(raiz, 140)){
+        printf("\nConsulta realizada com sucesso!");
+    }else{
+        printf("\nElemento nao encontrado...");
+    }
+
+}
